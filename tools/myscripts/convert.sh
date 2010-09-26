d06c745dd273c53a2895a304b090d523a143b2fb
Ahmed Mansour <atphalix@inbox.com>
2010-09-26 18:34:29 +0000

    Added GPL Licence & removed aggressive quit message

--- tools/myscripts/convert.sh
+++ tools/myscripts/convert.sh
@@ -1,10 +1,4 @@
-# convert *tga in directory to *jpg using imagemagick
-# usge: ./convert.sh [directory path]
-# $1 is the dir path
-# $1 is image format
-
-for img in $1*$2; do
-   base=`basename $img .$2`
-   echo "converting "$img
-   convert $img $1$base.jpg
-done
+# convert * images imagemagick
+# usage: ./convert.sh [source format destination format]
+# ex: ./convert gif png
+mogrify -format $2 *$1
