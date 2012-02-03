11dacf08394c4a9520db057a3aeddaf3116ea769
Ahmed Mansour <atphalix@inbox.com>
2012-02-03 21:20:27 +0000

    - Adapted Blender export script to Quake1. - removed ezquake source, replaced with sdlquake for olpc version. - removed useless textures.

--- ezquake_20_stable/masm32/download.bat
+++
@@ -1,2 +0,0 @@
-"../make/wget.exe" http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/windows-x86/ml.exe
-"../make/wget.exe" http://ezquake.svn.sourceforge.net/viewvc/*checkout*/ezquake/trunk/libs/windows-x86/gas2masm.exe
