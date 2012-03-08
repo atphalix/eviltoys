aa75fcd63d021ad4d23cf9ebe7241d1d9644ff4d
Ahmed Mansour <atphalix@inbox.com>
2012-03-08 21:08:44 +0000

    Rename Quake.activity into Eviltoys.activity, fix start map compile error and missing textures

--- Eviltoys.activity/bin/xrandr.sh
+++ Eviltoys.activity/bin/xrandr.sh
@@ -6,7 +6,7 @@ xrandr --addmode default 800x600
 xrandr --output default --mode 800x600
 # add below your activity:
 #$1
-cd /home/olpc/Activities/Quake.activity/bin
-./sdlquake -mem 32 -winsize 800 600
+
+./bin/nq-sgl -window -mem 32 -width 800 -height 600 +map start
 #go back to normal resolution mode
 xrandr --output default --mode 1200x900
