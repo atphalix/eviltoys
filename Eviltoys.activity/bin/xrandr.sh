ccb06df3a555a0707d99d37dbf79536fe18fe037
Ahmed Mansour <atphalix@inbox.com>
2012-03-16 15:25:30 +0000

    update 3D models

--- Eviltoys.activity/bin/xrandr.sh
+++ Eviltoys.activity/bin/xrandr.sh
@@ -7,6 +7,6 @@ xrandr --output default --mode 800x600
 # add below your activity:
 #$1
 
-./bin/nq-sgl -window -mem 32 -width 800 -height 600 +map start
+./sdlquake -window -mem 32 -width 800 -height 600 +map start
 #go back to normal resolution mode
 xrandr --output default --mode 1200x900
